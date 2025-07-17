8414e9c910ad92287116f0fb882eece48c9ef732
Muhammad Salman <salmancloud696@gmail.com>
2025-07-18 00:26:44 +0200

    Added styling to sign up button on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
     <div className="flex items-center justify-center flex-col">
       <h1 className={formStyles.title}>Project Management</h1>
     <Link href="/login">
-      <button className="bg-blue-600 text-white px-4 py-2 text-center rounded">
+      <button className="bg-blue-600 text-white px-4 py-2 text-center rounded w-48">
           Click here to Login
       </button>
                 
      </Link>
      <Link href="/signup">
-      <button className="bg-blue-600 text-white px-4 py-2 text-center rounded">
+      <button className="bg-blue-600 text-white px-4 py-2 text-center rounded w-48 mt-3">
           Click here to SignUp
       </button>
                 
